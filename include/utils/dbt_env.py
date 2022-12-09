5e9c1e91a9e523c0e5abcef733e8e7a7e8c4186b
Benji Lampel <benjamin@astronomer.io>
2022-12-09 16:44:46 -0500

    Fix project connection

--- include/utils/dbt_env.py
+++ include/utils/dbt_env.py
@@ -1,11 +1,13 @@
+import os
+
 dbt_env_vars = {
     "DBT_USER": "{{ conn.snowflake_default.login }}",
     "DBT_ENV_SECRET_PASSWORD": "{{ conn.snowflake_default.password }}",
     "DBT_HOST": "{{ conn.snowflake_default.host }}",
     "DBT_SCHEMA": "{{ conn.snowflake_default.schema }}",
     "OPENLINEAGE_URL": "https://benjisandbox.datakin.com",
-    "OPENLINEAGE_API_KEY": "{{ var.value.get('OPENLINEAGE_API_KEY') }}",
-    "SNOWFLAKE_ACCOUNT": "{{ var.value.get('SNOWFLAKE_ACCOUNT') }}",
+    "OPENLINEAGE_API_KEY": os.env.get("OPENLINEAGE_API_KEY"),
+    "SNOWFLAKE_ACCOUNT": os.env.get("SNOWFLAKE_ACCOUNT"),
     "SNOWFLAKE_USER": "{{ conn.snowflake_default.login }}",
     "SNOWFLAKE_PASSWORD": "{{ conn.snowflake_default.password }}",
     "ENV": "sandbox"
